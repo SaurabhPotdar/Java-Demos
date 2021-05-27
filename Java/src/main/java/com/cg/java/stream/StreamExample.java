f42b33bc06a79d2cccaaf8b50c9e404367411011
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2021-05-27 11:51:21 +0530

    Update StreamExample.java

--- Java/src/main/java/com/cg/java/stream/StreamExample.java
+++ Java/src/main/java/com/cg/java/stream/StreamExample.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.IntSummaryStatistics;
 import java.util.List;
-import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
